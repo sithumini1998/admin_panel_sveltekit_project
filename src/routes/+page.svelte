10c57e4ea0975a45d389e3357977e201356792af
sithumini1998 <98685539+sithumini1998@users.noreply.github.com>
2024-01-29 09:19:47 +0530

    first commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div class="flex flex-row h-dvh ">
+   <div class="basis-2/5 bg-blue-500 flex flex-col relative ">
+     <div class="absolute top-80 left-1/4">welcome to page</div>
+     <div class="absolute top-1/2 left-1/2">sign in to continue access</div>
+   </div>
+   <div class="  basis-3/5 relative bg-white ">
+     <div class="flex flex-col justify-center h-auto w-1/3 top-1/3 left-1/3 absolute ">
+        <h2 class="text-2xl font-bold mb-6 text-center">Sign In</h2>
+        <div class="mb-1">
+            <label for="username" class="block text-sm font-medium text-gray-600">User Name</label>
+            <input type="text" id="username" name="username" class="mt-1  w-full  border border-black text-xs p-1  text-center" placeholder="Enter your username">
+        </div>
+        <div class="mb-1">
+            <label for="username" class="block text-sm font-medium text-gray-600">User Id</label>
+            <input type="text" id="username" name="username" class="mt-1  w-full  border border-black text-xs p-1  text-center" placeholder="Enter your user Id">
+        </div>
+        <button class="bg-blue-500 text-white p-1  w-full mt-4">Login</button>
+        <p class="text-center font-medium mt-1">or</p>
+        <button class="bg-blue-500 text-white p-1  w-full mt-4">sign in with google</button>
+
+        
+     </div>
+     
+   </div>
+</div>
